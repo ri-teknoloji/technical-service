47812241c4b6a917e261b5874463dc1db24c73d6
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2024-10-03 23:56:57 +0300

    Refactor build scripts in package.json and vite.config.ts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import generouted from "@generouted/react-router/plugin";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   cacheDir: ".vite",
+  plugins: [react(), generouted()],
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-  plugins: [react(), generouted()],
 });
